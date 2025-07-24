c414497ba9490211867c485d112e3985128c72eb
ABHISHEKGUPTA-1234 <abhi8929314120@gmail.com>
2025-07-24 17:05:19 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FileStorage from './FileStorage.jsx';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <FileStorage />
+    </div>
+  );
+}
+export default App;
